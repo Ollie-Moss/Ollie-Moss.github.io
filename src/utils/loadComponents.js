51634cebdf84761b50415182ce64466b2741aa0d
Ollie Moss <olliemoss321@gmail.com>
2024-09-04 23:57:55 +1200

    Added basic folder structure with header, and footer

---
+++ src/utils/loadComponents.js
@@ -0,0 +1,20 @@
+function loadComponent(elementId, filePath) {
+    fetch(filePath)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(
+                    `Failed to load ${filePath}: ${response.statusText}`
+                )
+            }
+            return response.text()
+        })
+        .then((data) => {
+            document.getElementById(elementId).innerHTML = data
+        })
+        .catch((error) => console.error('Error loading component:', error))
+}
+
+window.onload = () => {
+    loadComponent('header', '../components/header/header.html')
+    loadComponent('footer', '../components/footer/footer.html')
+}
