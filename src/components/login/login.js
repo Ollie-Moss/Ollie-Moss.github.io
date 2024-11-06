ba6a53234b95e42c0946773ac47588542ee89172
Ollie Moss <olliemoss321@gmail.com>
2024-11-06 17:15:19 +1300

    Signup page

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,8 +1,12 @@
 window.addEventListener('loaded-components', async () => {
     const loginForm = document.getElementById('login-form')
+    const loginWithGoogleButtons = document.getElementsByClassName('login-with-google')
+    for (const btn of loginWithGoogleButtons) {
+        btn.addEventListener('click', loginWithGoogle)
+    }
     loginForm.addEventListener('submit', login)
 
-    function login(e){
+    function login(e) {
         e.stopPropagation()
         e.preventDefault()
 
@@ -10,19 +14,53 @@ window.addEventListener('loaded-components', async () => {
         // and associates them as a key value pair {ElemName: Value}
         // all elements without a name attribute are omitted
         const data = new FormData(e.target)
-        const [emailInput, passwordInput] = data.values()
-        loginWithUserAndPass(emailInput, passwordInput);
+        let [emailInput, passwordInput, rememberMeInput] = data.values()
+        rememberMeInput = rememberMeInput === 'on'
+
+        setAuthPersistence(rememberMeInput)
+        loginWithUserAndPass(emailInput, passwordInput)
     }
 
-    function loginWithUserAndPass(email, password){
-        auth.signInWithEmailAndPassword(email, password)
-            .then(result => {
+    function loginWithGoogle() {
+        const provider = new firebase.auth.GoogleAuthProvider()
+
+        auth.signInWithRedirect(provider)
+        auth.getRedirectResult()
+            .then((result) => {
                 const user = result.user
+                window.location.href = "/pages/index.html";
             })
-            .catch(error => {
+            .catch((error) => {
+                console.log(error.code)
+                throw new Error(error.message)
+            })
+    }
+
+    function loginWithUserAndPass(email, password) {
+        auth.signInWithEmailAndPassword(email, password)
+            .then((result) => {
+                const user = result.user
+                window.location.href = "/pages/index.html";
+            })
+            .catch((error) => {
                 console.log(error.code)
                 //TODO: handle error according to code
                 throw new Error(error.message)
+            })
+    }
+    function setAuthPersistence(local) {
+        const state = local
+            ? firebase.auth.Auth.Persistence.LOCAL
+            : firebase.auth.Auth.Persistence.SESSION
+        auth.setPersistence(state)
+            .then(() => {
+                console.log(
+                    'Firebase Auth persistence set to ' +
+                        (local ? 'LOCAL' : 'SESSION')
+                )
+            })
+            .catch((error) => {
+                console.error('Error setting persistence:', error)
             })
     }
 })
--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -1,8 +1,24 @@
 window.addEventListener('loaded-components', async () => {
+    let password_match = false
     const signupForm = document.getElementById('signup-form')
+    const passwordInput = document.getElementById('passwordInput')
+    const re_passwordInput = document.getElementById('re-passwordInput')
+
+    const loginWithGoogleButtons =
+        document.getElementsByClassName('login-with-google')
+
+    for (const btn of loginWithGoogleButtons) {
+        btn.addEventListener('click', loginWithGoogle)
+    }
     signupForm.addEventListener('submit', signup)
 
-    function signup(e){
+    re_passwordInput.addEventListener('input', (e) => {
+        if (e.target.value != '' || passwordInput.value != '') {
+            password_match = e.target.value == passwordInput.value
+        }
+    })
+
+    function signup(e) {
         e.stopPropagation()
         e.preventDefault()
 
@@ -10,17 +26,35 @@ window.addEventListener('loaded-components', async () => {
         // and associates them as a key value pair {ElemName: Value}
         // all elements without a name attribute are omitted
         const data = new FormData(e.target)
-        const [emailInput, passwordInput] = data.values()
-        signupWithUserAndPass(emailInput, passwordInput);
+        let [emailInput, passwordInput] = data.values()
+        if (password_match) {
+            console.log("here")
+            signUpWithUserAndPass(emailInput, passwordInput)
+        }
     }
 
-    function signupWithUserAndPass(email, password){
-        auth.createUserWithEmailAndPassword(email, password)
-            .then(result => {
+    function loginWithGoogle() {
+        const provider = new firebase.auth.GoogleAuthProvider()
+
+        auth.signInWithRedirect(provider)
+        auth.getRedirectResult()
+            .then((result) => {
                 const user = result.user
-                console.log(user)
+                window.location.href = '/pages/index.html'
             })
-            .catch(error => {
+            .catch((error) => {
+                console.log(error.code)
+                throw new Error(error.message)
+            })
+    }
+
+    function signUpWithUserAndPass(email, password) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .then((result) => {
+                const user = result.user
+                window.location.href = '/pages/index.html'
+            })
+            .catch((error) => {
                 console.log(error.code)
                 //TODO: handle error according to code
                 throw new Error(error.message)
